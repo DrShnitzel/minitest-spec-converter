1d26418ffccf27568b3a7d2e253c6abb4a12f05e
Aleksei Zaitsev <drshnitsel@gmail.com>
2024-07-22 16:56:47 +0400

    Initial commit

---
+++ lib/minitest_spec_converter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "minitest_spec_converter/version"
+
+module MinitestSpecConverter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/minitest_spec_converter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MinitestSpecConverter
+  VERSION = "0.1.0"
+end
